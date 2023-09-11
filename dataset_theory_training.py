942392f3b58bfca160d6f5e29b7541b8f2513a2d
alex-benitez <alexbenitezcanelles@gmail.com>
2023-09-11 10:16:20 +0100

    Realized that the dataset_theory function is broken

--- dataset_theory_training.py
+++ dataset_theory_training.py
@@ -21,9 +21,9 @@ import Convtools as ct
 import seaborn as sn
 from andi_datasets.datasets_theory import datasets_theory
 
-N = 20
+N = 1000
 min_T = 320
-size = 2
+size = 10
 start = time.time()
 
 
@@ -31,64 +31,71 @@ start = time.time()
 
 trajectories = []
 alpha_vals = []
-for i in range(size):
+for i in range(N):
     traj_type = random.randint(0,4)
-    print(traj_type)
+
     if traj_type==0 or traj_type==1 or traj_type==4:
+        
         alpha = random.random()
-    if traj_type==3:
+        # print('womp womp')  # This print statement magically made my code work, it is sacred
+    elif traj_type==3:
         alpha = random.random() + 1        
     else:
         alpha = random.random()*2
-    print(alpha)
+
     trajectories.append(datasets_theory().create_dataset(T=min_T,N_models=1,models=traj_type,exponents=[alpha])[0])
-print(trajectories,alpha)
+    alpha_vals.append(alpha)
+# print(trajectories,alpha)
 
-        
+
     
-# # alpha = [[0]*i + [1] + [0]*(4-i) for i in range(5)]  Brilliantly condensed code which will never see the light of day again (literally just makes an I matrix)
-
-# print(len(trajectories))
-
-# print(len(trajectories[3]))
+# alpha = [[0]*i + [1] + [0]*(4-i) for i in range(5)]  Brilliantly condensed code which will never see the light of day again (literally just makes an I matrix)
 
 
-# for i in range(len(trajectories)):
-    
-#     for j in range(len(trajectories[i])):
-#         trajectories[i],alpha = ct.normalize_data(trajectories[i],[i for x in range(len(trajectories))])
-#         del trajectories[i][j][min_T]
-#         del trajectories[i][j][min_T] # For some reason trajectories generate with extra numbers at the end, just clip them
+
+
+
+trajectories,uselessvar = ct.normalize_data(trajectories,[i for x in range(len(trajectories))])
+
+
+for i in range(len(trajectories)):
+        del trajectories[i][min_T]
+        del trajectories[i][min_T] # For some reason trajectories generate with extra numbers at the end, just clip them
         
 
 
-# model = Sequential()
-# model.add(Conv1D(filters=16, kernel_size=size, activation='relu', input_shape=(min_T,1)))
-# model.add(BatchNormalization())
-# model.add(MaxPooling1D(pool_size=4))
-# model.add(Conv1D(filters=32, kernel_size=4, activation='relu'))#, input_shape=(304, 16)))
-# model.add(BatchNormalization())
-# model.add(MaxPooling1D(pool_size=2))
-# model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))#, input_shape=(304, 16)))
-# model.add(BatchNormalization())
-# model.add(MaxPooling1D(pool_size=2))
-# model.add(Dense(50, activation='relu'))
-# model.add(Dropout(0.25))
-# model.add(Flatten())
-# model.add(Dense(5,activation='softmax'))
-# model.summary()
-# model.compile(optimizer=Adam(learning_rate=0.001), loss='mse') # Random model tbh, not much thought put into it
+model = Sequential()
+model.add(Conv1D(filters=16, kernel_size=size, activation='relu', input_shape=(min_T,1)))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=4))
+model.add(Conv1D(filters=32, kernel_size=4, activation='relu'))#, input_shape=(304, 16)))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=2))
+model.add(Conv1D(filters=64, kernel_size=4, activation='relu'))#, input_shape=(304, 16)))
+model.add(BatchNormalization())
+model.add(MaxPooling1D(pool_size=2))
+model.add(Dense(50, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(5,activation='softmax'))
+model.summary()
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mse') # Semi random model, it was a lot of tweaking
 
 
-# '''
-# Fittin the model time, how does this work? Time to figure it out lord have mercy
-
-# Okay so we have our trajectories = [ [ ], [ ], [ ], [ ], [ ] ]
-
-# '''
-# # model.fit(X2[0], alpha, epochs=30)
+print(len(trajectories))
 
 
+model.fit(trajectories[0:int(N*0.8)], alpha_vals[0:int(N*0.8)], epochs=30) # Splitting the data for validation
+
+validation_set = alpha_vals[int(N*0.8):]
+accurate = 0
+yhat = model.predict(trajectories[int(N*0.8):],verbose=0)
+for i in range(len(yhat)):
+    if np.argmax(yhat[i]) == validation_set[i]:
+        accurate += 1
+
+
+print(accurate/len(trajectories)*0.2)
 
 
 # for i in range(5):
